a0f0e4262c8b47b6e9d3590e67112efd4cf4b0a6
Derek <darmfield2023@gmail.com>
2023-03-18 15:12:54 -0400

    created functionality to load, flatten, normalize, and batch mnist dataset

---
+++ load_data.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import numpy as np
+
+def loadMnistDataset(batch_size=64, input_shape=(28, 28)):
+    # Load in MNIST dataset as numpy array
+    (train_X, train_y), (test_X, test_y) = tf.keras.datasets.mnist.load_data()
+
+    # noramlizes and flattens dataset in numpy array
+    flattened = np.reshape(train_X/255.0, [len(train_X), input_shape[0] * input_shape[1]])
+
+    # Loads np array into a Dataset object
+    train_X_ds = tf.data.Dataset.from_tensor_slices(flattened)
+
+    # Returns the flattened, normalized, batched MNIST dataset
+    return train_X_ds.batch(batch_size)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
